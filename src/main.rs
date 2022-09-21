dc28d47d5c60b46289159f22409873c2996e7a28
Conner Bondurant <conner.bondurant@triptych.me>
2022-09-21 16:04:25 -0400

    Actually save the rust fmt changes...

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-
-
 //TODO: Remove once out of early prototyping
 #![allow(dead_code)]
 mod mdparser;
@@ -7,40 +5,60 @@ mod mdparser;
 use mdparser::MDParser;
 
 fn main() {
-
 	let parser = MDParser::parse(
-"# This is my level 1 header
+		"# This is my level 1 header
 ## This is my level 2 header
 
 Wow!!!
 This is a test paragraph!
 *and* a continuation
 
-Second Paragraph");
+Second Paragraph",
+	);
 
-	for token in parser{
+	for token in parser {
 		println!("{:?}", token);
 	}
 }
 
 #[cfg(test)]
-mod tests{
-    use crate::mdparser::{MDParser, Token};
-
+mod tests {
+	use crate::mdparser::{MDParser, Token};
 
 	// It is my belief that tests have no need to be pretty, they are far too functional for that.
 	#[test]
 	fn header_parsing() {
-		assert_eq!(Token::Heading(1, "Test".to_string()), MDParser::parse("# Test").into_iter().next().unwrap());
-		assert_eq!(Token::Heading(2, "Test".to_string()), MDParser::parse("## Test").into_iter().next().unwrap());
-		assert_eq!(Token::Heading(3, "Test".to_string()), MDParser::parse("### Test").into_iter().next().unwrap());
-		assert_eq!(Token::Heading(4, "Test".to_string()), MDParser::parse("#### Test").into_iter().next().unwrap());
-		assert_eq!(Token::Heading(5, "Test".to_string()), MDParser::parse("##### Test").into_iter().next().unwrap());
-		assert_eq!(Token::Heading(6, "Test".to_string()), MDParser::parse("###### Test").into_iter().next().unwrap());
+		assert_eq!(
+			Token::Heading(1, "Test".to_string()),
+			MDParser::parse("# Test").into_iter().next().unwrap()
+		);
+		assert_eq!(
+			Token::Heading(2, "Test".to_string()),
+			MDParser::parse("## Test").into_iter().next().unwrap()
+		);
+		assert_eq!(
+			Token::Heading(3, "Test".to_string()),
+			MDParser::parse("### Test").into_iter().next().unwrap()
+		);
+		assert_eq!(
+			Token::Heading(4, "Test".to_string()),
+			MDParser::parse("#### Test").into_iter().next().unwrap()
+		);
+		assert_eq!(
+			Token::Heading(5, "Test".to_string()),
+			MDParser::parse("##### Test").into_iter().next().unwrap()
+		);
+		assert_eq!(
+			Token::Heading(6, "Test".to_string()),
+			MDParser::parse("###### Test").into_iter().next().unwrap()
+		);
 	}
 
 	#[test]
 	fn paragraph_parsing() {
-		assert_eq!(Token::Paragraph(vec![Token::Text("Hi".to_string())]), MDParser::parse("Hi").into_iter().next().unwrap());
+		assert_eq!(
+			Token::Paragraph(vec![Token::Text("Hi".to_string())]),
+			MDParser::parse("Hi").into_iter().next().unwrap()
+		);
 	}
 }
--- src/mdparser.rs
+++ src/mdparser.rs
@@ -5,7 +5,7 @@ pub enum Token {
 	Blockquote(String),
 	Link { href: String, text: String },
 	Codeblock { lang: String, text: String },
-	Text (String),
+	Text(String),
 	Done,
 }
 
@@ -15,20 +15,19 @@ pub struct MDParser<'a> {
 }
 
 impl<'a> Iterator for MDParser<'a> {
-
 	type Item = Token;
 
 	fn next(&mut self) -> Option<Token> {
-		if let Some(index) = self.markdown[self.index..].find(|c: char| !c.is_whitespace()){
+		if let Some(index) = self.markdown[self.index..].find(|c: char| !c.is_whitespace()) {
 			self.index += index; // Index is relative now
-		}else{
+		} else {
 			return None;
 		}
 		let leading_grapheme = MDParser::get_leading_grapheme(&self.markdown[self.index..]);
 
 		// Mutate the index internally here, instead of having to manage passing it in and out of the
 		// function call
-		match leading_grapheme{
+		match leading_grapheme {
 			// Theres gotta be a better way here but hell if I know what it is.
 			"#" => Some(self.consume_header()),
 			"##" => Some(self.consume_header()),
@@ -36,14 +35,13 @@ impl<'a> Iterator for MDParser<'a> {
 			"####" => Some(self.consume_header()),
 			"#####" => Some(self.consume_header()),
 			"######" => Some(self.consume_header()),
-			_ => Some(self.consume_paragraph())
+			_ => Some(self.consume_paragraph()),
 		}
 	}
 }
 
 impl<'a> MDParser<'a> {
-
-	pub fn parse(text: &str) -> MDParser{
+	pub fn parse(text: &str) -> MDParser {
 		MDParser {
 			markdown: text,
 			index: 0,
@@ -51,52 +49,56 @@ impl<'a> MDParser<'a> {
 	}
 
 	fn get_leading_grapheme(text: &str) -> &str {
-		if let Some(back_index) = text.find(char::is_whitespace){
+		if let Some(back_index) = text.find(char::is_whitespace) {
 			&text[..back_index]
-		}else{
+		} else {
 			""
 		}
 	}
 
 	fn consume_header(&mut self) -> Token {
 		let mut header_level = 0;
-		while Some("#") == self.markdown[self.index..].get(..1){
+		while Some("#") == self.markdown[self.index..].get(..1) {
 			header_level += 1;
 			self.index += 1;
 		}
 
-		self.index += self.markdown[self.index..].find(|c:char| !c.is_whitespace()).unwrap_or(0);
+		self.index += self.markdown[self.index..]
+			.find(|c: char| !c.is_whitespace())
+			.unwrap_or(0);
 
 		let start = self.index;
 		// I think headers should only be one line.
-		let end = self.markdown[self.index..].find('\n').unwrap_or_else(|| self.markdown[self.index..].len());
+		let end = self.markdown[self.index..]
+			.find('\n')
+			.unwrap_or_else(|| self.markdown[self.index..].len());
 		self.index += end;
-		Token::Heading(header_level, self.markdown[start..start+end].to_string())
+		Token::Heading(header_level, self.markdown[start..start + end].to_string())
 	}
 
 	fn consume_paragraph(&mut self) -> Token {
 		let start = self.index;
 		// TODO: remove the need to both iterate for find and then iterate in the replace.
 
-		if let Some(end) = self.markdown[self.index..].find("\n\n"){
+		if let Some(end) = self.markdown[self.index..].find("\n\n") {
 			self.index += end;
-			MDParser::parse_paragraph_internals(&self.markdown[start..start+end])
-		}else{
+			MDParser::parse_paragraph_internals(&self.markdown[start..start + end])
+		} else {
 			self.index = self.markdown.len(); // This paragraph is the final bit of text, reached end of text.
 			MDParser::parse_paragraph_internals(&self.markdown[start..])
 		}
 	}
 
-	fn parse_paragraph_internals(text: &str)-> Token {
+	fn parse_paragraph_internals(text: &str) -> Token {
 		let mut contents = Vec::new();
 
 		let mut text_split = text.split('\n');
 
 		let mut new_text = String::new();
-		if let Some(start) = text_split.next(){
+		if let Some(start) = text_split.next() {
 			new_text.push_str(start);
 
-			for line in text_split{
+			for line in text_split {
 				new_text.push(' ');
 				new_text.push_str(line.trim());
 			}
@@ -105,5 +107,4 @@ impl<'a> MDParser<'a> {
 		contents.push(body);
 		Token::Paragraph(contents)
 	}
-
 }
